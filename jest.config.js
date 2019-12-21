9c45b792dd08fb6ebd3a15d46225ddb09df33909
Winnie Ho <winnie.ho@devfacto.com>
2019-12-21 00:02:30 -0700

    Add style mock

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   verbose: true,
   moduleDirectories: ['node_modules', 'src/components', 'src/environments'],
   moduleNameMapper: {
-    'environment': '<rootDir>/src/environments/development.js'
+    'environment': '<rootDir>/src/environments/development.js',
+    '\\.(css|scss)': '<rootDir>/__mocks__/styleMock.js'
   }
 }
